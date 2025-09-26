5db62f0ea9f8d1430c9a09ccd57516495884c946
DimasAnjayMabar <gregoriusnovantino17@gmail.com>
2025-09-26 13:19:11 +0700

    upload

--- src/components/PortfolioData.tsx
+++ src/components/PortfolioData.tsx
@@ -36,7 +36,7 @@ export const websiteTypes: PortfolioItem[] = [
   {
     id: 4,
     title: "Simple Cooperation app",
-    description: "A demo for cooperation app to help cooperation staff manage their members. try to use username = greg and password = 123456 for login. profile is not working because using vercel's static page",
+    description: "A demo for cooperation app to help cooperation staff manage their members. \ntry to use username = greg and password = 123456 for login. \nprofile is not working because using vercel's static page",
     image: "/png/koperasi.png",
     link: "https://koperasiapp-qskb4jcnq-gregs-projects-44e86848.vercel.app",
     github: "https://github.com/DimasAnjayMabar/koperasi.git"
